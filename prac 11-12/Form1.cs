7e4285990d8c0b0abd695c49ea5f9d25dac0bb20
BulatDes <bulatdespizek@gmail.com>
2023-02-10 00:57:17 +0500

    first commit

---
+++ prac 11-12/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_11_12
+{
+    public partial class Student_work : Form
+    {
+        public Student_work()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student stud = new Student();
+            stud.name = textBox1.Text;
+            stud.rost = (int)numericUpDown1.Value;
+           // stud.ves = (int)numericUpDown2.Value;
+            stud.eda = (double)numericUpDown3.Value;
+            MessageBox.Show(string.Format("Студент: {0} \nрост: {1} \nвес: {2}", stud.name, stud.rost, stud.GetEat()));
+            stud.SetEat(stud.eda); 
+            MessageBox.Show(string.Format("Студент съел {3} кг еды: {0} \nрост: {1} \nвес: {2}", stud.name, stud.rost, stud.GetEat(),stud.eda));
+           
+
+
+            //рассчет веса, если студент съел 5 кг
+
+        }
+    }
+}
---
+++ prac 11-12/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_11_12
+{
+    class Student
+    {
+        public string name;
+        public double rost;
+        internal double ves;
+        public double eda;
+
+        public void SetEat(double eda)
+        {
+            if (eda > 5 && eda < 10)
+            {
+                ves = ves + ((eda * 1000 - 1600) / 1000)%70;
+                rost--;
+            }
+            else if (eda >= 10)
+            {
+                ves = ves + ((eda * 1000 - 1600) / 1000) % 50;
+                rost-=2;
+            }
+            else
+                ves = ves + (eda * 1000 - 1600) / 1000;
+        }
+        public double GetEat()
+        {
+            return ves;
+        }
+
+    }
+}
